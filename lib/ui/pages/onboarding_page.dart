768a31a8d61df3e1779ef377033eb481115a8e52
shandikadav <shandikadavid.ardiansyah@students.amikom.ac.id>
2023-12-19 02:21:05 +0700

    adding onboarding page

---
+++ lib/ui/pages/onboarding_page.dart
@@ -0,0 +1,175 @@
+import 'package:bank_sha/shared/theme/theme.dart';
+import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+
+class OnboardingPage extends StatefulWidget {
+  const OnboardingPage({super.key});
+
+  @override
+  State<OnboardingPage> createState() => _OnboardingPageState();
+}
+
+class _OnboardingPageState extends State<OnboardingPage> {
+  int currentIndex = 0;
+  CarouselController carouselController = CarouselController();
+
+  List<String> titles = [
+    'Grow Your\nFinancial Today',
+    'Build From\nZero to Freedom',
+    'Start Together',
+  ];
+
+  List<String> subtitles = [
+    'Our system is helping you to\nachieve a better goal',
+    'We provide tips for you so that\nyou can adapt easier',
+    'We will guide you to where\nyou wanted it too',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: lightBackgroundColor,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CarouselSlider(
+              items: [
+                Image.asset(
+                  'assets/img_onboarding1.png',
+                  height: 331,
+                ),
+                Image.asset(
+                  'assets/img_onboarding2.png',
+                  height: 331,
+                ),
+                Image.asset(
+                  'assets/img_onboarding3.png',
+                  height: 331,
+                ),
+              ],
+              options: CarouselOptions(
+                height: 331,
+                viewportFraction: 1,
+                enableInfiniteScroll: false,
+                onPageChanged: (index, reason) {
+                  setState(() {
+                    currentIndex = index;
+                  });
+                },
+              ),
+              carouselController: carouselController,
+            ),
+            const SizedBox(
+              height: 80,
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(
+                horizontal: 24,
+              ),
+              padding: const EdgeInsets.symmetric(
+                horizontal: 22,
+                vertical: 24,
+              ),
+              decoration: BoxDecoration(
+                color: whiteColor,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Column(
+                children: [
+                  Text(
+                    titles[currentIndex],
+                    style: blackTextStyle.copyWith(
+                      fontSize: 20,
+                      fontWeight: semiBold,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(
+                    height: 26,
+                  ),
+                  Text(
+                    subtitles[currentIndex],
+                    style: greyTextStyle.copyWith(
+                      fontSize: 16,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(
+                    height: 50,
+                  ),
+                  Row(
+                    children: [
+                      Container(
+                        width: 12,
+                        height: 12,
+                        margin: const EdgeInsets.only(
+                          right: 10,
+                        ),
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: currentIndex == 0
+                              ? blueColor
+                              : lightBackgroundColor,
+                        ),
+                      ),
+                      Container(
+                        width: 12,
+                        height: 12,
+                        margin: const EdgeInsets.only(
+                          right: 10,
+                        ),
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: currentIndex == 1
+                              ? blueColor
+                              : lightBackgroundColor,
+                        ),
+                      ),
+                      Container(
+                        width: 12,
+                        height: 12,
+                        margin: const EdgeInsets.only(
+                          right: 10,
+                        ),
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: currentIndex == 2
+                              ? blueColor
+                              : lightBackgroundColor,
+                        ),
+                      ),
+                      const Spacer(),
+                      Container(
+                        width: 150,
+                        height: 50,
+                        child: TextButton(
+                          onPressed: () {
+                            carouselController.nextPage();
+                          },
+                          style: TextButton.styleFrom(
+                            backgroundColor: purpleColor,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(65),
+                            ),
+                          ),
+                          child: Text(
+                            'Continue',
+                            style: whiteTextStyle.copyWith(
+                              fontSize: 16,
+                              fontWeight: semiBold,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
