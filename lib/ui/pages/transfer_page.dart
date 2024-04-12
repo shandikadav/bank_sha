7cdfc0fe10904be839f3998efe4963500bb640d2
shandikadav <shandikadavid.ardiansyah@students.amikom.ac.id>
2024-04-12 14:54:10 +0700

    feat : add buildresult widget

--- lib/ui/pages/transfer_page.dart
+++ lib/ui/pages/transfer_page.dart
@@ -1,6 +1,8 @@
 import 'package:bank_sha/shared/theme/theme.dart';
+import 'package:bank_sha/ui/widgets/buttons.dart';
 import 'package:bank_sha/ui/widgets/forms.dart';
 import 'package:bank_sha/ui/widgets/transfer_recent_user_item.dart';
+import 'package:bank_sha/ui/widgets/transfer_result_user_item.dart';
 import 'package:flutter/material.dart';
 
 class TransferPage extends StatelessWidget {
@@ -32,7 +34,18 @@ class TransferPage extends StatelessWidget {
             title: 'by username',
             isShowTitle: false,
           ),
-          buildRecentUsers(),
+          //buildRecentUsers(),
+          buildResult(),
+          const SizedBox(
+            height: 214,
+          ),
+          CustomFilledButton(
+            title: 'Continue',
+            onPressed: () {},
+          ),
+          const SizedBox(
+            height: 50,
+          ),
         ],
       ),
     );
@@ -84,6 +97,40 @@ Widget buildResult() {
   return Container(
     margin: const EdgeInsets.only(
       top: 40,
+    ),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'Result',
+          style: blackTextStyle.copyWith(
+            fontSize: 16,
+            fontWeight: semiBold,
+          ),
+        ),
+        const SizedBox(
+          height: 14,
+        ),
+        Wrap(
+          spacing: 17,
+          runSpacing: 17,
+          children: [
+            TransferResultUserItem(
+              imageUrl: 'assets/img_friend1.png',
+              name: 'Yonna Jie',
+              username: 'yoenna',
+              isVerified: true,
+            ),
+            TransferResultUserItem(
+              imageUrl: 'assets/img_friend2.png',
+              name: 'Yonne Ka',
+              username: 'yoenyu',
+              isVerified: true,
+              isSelected: true,
+            ),
+          ],
+        )
+      ],
     ),
   );
 }
